3d713fb53f25461ecd9df25f39d8a41b78d485ca
Drac Zihper <draczihper@gmail.com>
2024-11-06 20:12:40 +0300

    Add game board initial test

--- ship.test.js
+++ ship.test.js
@@ -34,3 +34,13 @@ describe("ships factory test", () => {
   });
 });
 
+
+describe("game board factory test", () => {
+  test("game board initialization", () => {
+    for (let i = 0; i < 10; i++){
+      for (let j = 0; j < 10; j++){
+        console.log(i,j)
+      }
+    }
+  })
+})
