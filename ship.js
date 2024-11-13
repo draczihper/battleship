aa354651991e50ac941ddcdc4126227f094ac156
Drac Zihper <draczihper@gmail.com>
2024-11-13 20:26:59 +0300

    Add game board grid

--- ship.js
+++ ship.js
@@ -16,66 +16,14 @@ function Ship(length) {
   }
 }
 
- // Random coordinates in a ten by ten grid
- const randomNum = () => {
-  const num = Math.floor(Math.random() * 10);
-  return num;
-}
-const shipX = randomNum();
-const shipY = randomNum();
-
-const randomOrientation = () => {
-  const num = Math.floor(Math.random() * 2);
-  return num === 0 ? "horizontal" : "vertical"
-}
-const orientation = randomOrientation();
-
-
-// Place ships at a specific coordinate 
-function placeShip(x, y, len, orientation) {
-  const grid = Array(10).fill(0).map(() => Array(10).fill("."));
-
-if (orientation === "horizontal" && shipX + shipLength <= 10) {
-  for (let i = 0; i < shipLength; i++){
-    grid[shipY][shipX + i] = "0";
-  }
-} else if(orientation === "vertical" && shipX + shipLength <= 10) {
-  for (let i = 0; i < shipLength; i++){
-    grid[shipX + i][shipY] = "0";
-  }
-} else {
-  console.log("This ship cannot be placed here without overflowing!")
-  return;
-}
-
-const output = grid.map(row => row.join(" ")).join("\n");
-
-return output;
-}
-
-
 function Gameboard() {
   const grid = Array(10).fill(0).map(() => Array(10).fill('.'));
   const missedAttacks = [];
   const ships = [];
 
-
-
- 
-  return {
-    shipsArray.forEach(ship => {
-      placeShip(shipX, shipY, ship.length, orientation);
-    });
-  }
+  
 }
 
 module.exports = {
-  carrier,
-  battleship,
-  submarine,
-  destroyer,
-  cruiser,
-  hit, 
-  isSunk,
-  Gameboard,
+ 
 }
