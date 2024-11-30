88b6db3afedaf89be1eb368ff7a57c81715aa1dd
Drac Zihper <draczihper@gmail.com>
2024-11-30 11:51:09 +0300

    Add make random move test case

--- ship.test.js
+++ ship.test.js
@@ -122,8 +122,12 @@ describe("player factory test", () => {
   test("player type", () => {
     const human = Player("human");
     const computer = Player("computer")
-
     expect(human.type).toEqual("human");
     expect(computer.type).toEqual("computer");
   });
+  test("test making a random move", () => {
+    const human = Player("human");
+
+    expect(isNaN(human.makeRandomMove())).toEqual(true);
+  })
 });
