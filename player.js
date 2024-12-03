5ec8a1c0b1ee9dc23f3a6c4e02f589c952f624a8
Drac Zihper <draczihper@gmail.com>
2024-12-03 21:47:50 +0300

    Modify placeships randomly and import statements

--- player.js
+++ player.js
@@ -1,24 +1,25 @@
-const {Ship, Gameboard} = require("./ship.js")
+import { Ship, Gameboard } from "./ship.js";
 
 function Player(type = "human") {
   const gameboard = Gameboard();
 
-  const placeShipsRandomly = (ship) => {
+  const placeShipsRandomly = () => {
     const BOARD_SIZE = 10;
     const shipLengths = [5, 4, 3, 3, 2];
 
-    const ship = shipLengths.map(Ship);
-    while (true) {
-      const isVertical = Math.random() < 0.5;
-      const maxX = isVertical ? BOARD_SIZE - 1 : BOARD_SIZE - ship.length;
-      const maxY = isVertical ? BOARD_SIZE - ship.length : BOARD_SIZE - 1;
+    const ships = shipLengths.map(Ship);
+    let placed = false
+    ships.forEach(ship => {
+      while (!placed) {
+        const isVertical = Math.random() < 0.5;
+        const maxX = isVertical ? BOARD_SIZE - 1 : BOARD_SIZE - ship.length;
+        const maxY = isVertical ? BOARD_SIZE - ship.length : BOARD_SIZE - 1;
 
-      const x = Math.floor(Math.random() * (maxX + 1));
-      const y = Math.floor(Math.random() * (maxY + 1));
-      if (placeShip(x, y, ship, isVertical)) {
-        return true;
+        const x = Math.floor(Math.random() * (maxX + 1));
+        const y = Math.floor(Math.random() * (maxY + 1));
+        placed = gameboard.placeShip(x, y, ship, isVertical)
       }
-    }
+    });
   };
 
   const makeRandomMove = () => {
@@ -44,11 +45,10 @@ function Player(type = "human") {
   return {
     type,
     gameboard,
+    placeShipsRandomly,
     makeRandomMove,
     attack,
   };
 }
 
-module.exports = {
-  Player,
-};
+export default Player;
