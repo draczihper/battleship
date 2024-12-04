6cba07cf25ad37c1f5613fc87a4ef8e0ed73e896
Drac Zihper <draczihper@gmail.com>
2024-12-04 22:00:14 +0300

    Modify placeships randomly and make random attack

--- player.js
+++ player.js
@@ -12,42 +12,45 @@ function Player(type = "human") {
     ships.forEach(ship => {
       while (!placed) {
         const isVertical = Math.random() < 0.5;
+        const x = Math.floor(Math.random() * 10)
+        const y = Math.floor(Math.random() * 10)
+        /*
         const maxX = isVertical ? BOARD_SIZE - 1 : BOARD_SIZE - ship.length;
         const maxY = isVertical ? BOARD_SIZE - ship.length : BOARD_SIZE - 1;
 
         const x = Math.floor(Math.random() * (maxX + 1));
         const y = Math.floor(Math.random() * (maxY + 1));
+        */
         placed = gameboard.placeShip(x, y, ship, isVertical)
       }
     });
   };
 
-  const makeRandomMove = () => {
+  const makeRandomAttack = (enemyBoard) => {
     let x, y;
     do {
       x = Math.floor(Math.random() * 10);
       y = Math.floor(Math.random() * 10);
-    } while (
-      gameboard.getGrid()[y][x] === "H" ||
-      gameboard.getGrid()[y][x] === "X"
-    );
-    return { x, y };
+    } while (Array.from(enemyBoard.missedAttacks).some(
+      key => key === `${x},${y}`
+    ));
+    return { x, y, result: enemyBoard.receiveAttack(x, y) };
   };
 
-  const attack = (enemyBoard, x, y) => {
+/*   const attack = (enemyBoard, x, y) => {
     if (type === "computer") {
       const move = makeRandomMove();
       return enemyBoard.receiveAttack(move.x, move.y);
     }
     return enemyBoard.receiveAttack(x, y);
-  };
+  }; */
 
   return {
     type,
     gameboard,
     placeShipsRandomly,
-    makeRandomMove,
-    attack,
+    makeRandomAttack,
+    //attack,
   };
 }
 
